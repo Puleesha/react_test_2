7653f4872c1f3c3ae9d218145d79135c894a6726
tharukabandara2005 <tharuka.sathvidu@gmail.com>
2024-02-25 12:20:51 +0530

    dsads

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import LoginPage from './Components/LoginPage/LoginPage.js';
+import UserProfile from './Components/UserProfile/UserProfile.js';
+import AboutUs from './Components/AboutUsPage/AboutUsPage.js'
+import HomePage from './Components/HomePage/HomePage.js';
+import LocationPage from './Components/LocationPage/LocationPage.js';
+import { RegistrationPage } from './Components/RegistrationPage/RegistrationPage.js';
+import ErrorPage from './Components/ErrorPage/ErrorPage.js';
+import Colombo from './Components/LocationDescriptionPages/Colombo.js';
+import Galle from './Components/LocationDescriptionPages/Galle.js';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
