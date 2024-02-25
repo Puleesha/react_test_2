58cccbb9de766eeb485b7dfaae96245a09d21a5e
Puleesha <puleeshak@gmail.com>
2024-02-25 12:41:34 +0530

    wiefn

--- src/App.js
+++ src/App.js
@@ -9,30 +9,31 @@ import { RegistrationPage } from './Components/RegistrationPage/RegistrationPage
 import ErrorPage from './Components/ErrorPage/ErrorPage.js';
 import Colombo from './Components/LocationDescriptionPages/Colombo.js';
 import Galle from './Components/LocationDescriptionPages/Galle.js';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>My name is Puleesha.</p>
-    //     <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
-    //   </header>
-    // </div>
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/login" element={<LoginPage/>} />
-        <Route path="/profile" element={<UserProfile/>} />
-        <Route path="/register" element={<RegistrationPage/>} />
-        <Route path="/about" element={<AboutUs/>} />
-        <Route path="/location" element={<LocationPage/>}/>
-        <Route path = "*" element = {<ErrorPage/>}/>
-        <Route path="/colombo" element={<Colombo/>} />
-        <Route path="/galle" element={<Galle/>} />
-      </Routes>
-  </Router> 
+    <div className="App">
+      <Colombo/>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>My name is Puleesha.</p>
+        <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
+      </header> */}
+    </div>
+  //   <Router>
+  //     <Routes>
+  //       <Route path="/" element={<HomePage/>} />
+  //       <Route path="/login" element={<LoginPage/>} />
+  //       <Route path="/profile" element={<UserProfile/>} />
+  //       <Route path="/register" element={<RegistrationPage/>} />
+  //       <Route path="/about" element={<AboutUs/>} />
+  //       <Route path="/location" element={<LocationPage/>}/>
+  //       <Route path = "*" element = {<ErrorPage/>}/>
+  //       <Route path="/colombo" element={<Colombo/>} />
+  //       <Route path="/galle" element={<Galle/>} />
+  //     </Routes>
+  // </Router> 
   );
 }
 
